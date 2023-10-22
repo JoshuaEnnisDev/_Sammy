e575ddc2b2f5d2bd90b8ad6bcc5243694ae10615
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-10-21 23:06:43 -0500

    commit

--- Minecraft Catch Game/main.py
+++ Minecraft Catch Game/main.py
@@ -1,22 +1,25 @@
 import pgzrun
-import time
-WIDTH = 1000
-HEIGHT = 800
 
-start_time = time.time()
-current_time = 0
-print(start_time)
+WIDTH = 1000
+HEIGHT = 755
+
+time = 100
 
 
 def draw():
-    screen.blit("bg1", (0, 0))
-    screen.blit("grass1000", (0, 750))
-    screen.draw.text(f"{int(current_time)}", (10, 10))
+    screen.blit("bg3", (0, 0))
+    # screen.fill("skyblue")
+    screen.blit("grass1000", (0, 700))
+    # screen.blit("01ground", (0, 0))
+    # screen.blit("03distant_trees", (0, 0))
+    # screen.blit("02trees", (0, 0))
+
+    screen.draw.text(f"{int(time)}", (10, 10), fontsize=45)
 
 
 def update():
-    global current_time
-    current_time = time.time() - start_time
+    global time
+    time -= 1 / 60
 
 
 pgzrun.go()
